04d80a409b2be09684561ca4a0f65a321bfc9496
neto.lx@gmail.com <neto.lx@gmail.com@d583e3dc-27d8-11df-bfd4-992fa37f6226>
2010-05-20 21:52:05 +0000

    Proposta de Alteração à REV61

--- trunk/Src/EDM/EDM.FoundationClasses/FoundationType/Validator.cs
+++ trunk/Src/EDM/EDM.FoundationClasses/FoundationType/Validator.cs
@@ -4,117 +4,61 @@ using EDM.FoundationClasses.Exception.FoundationType;
 
 namespace EDM.FoundationClasses.FoundationType
 {
-    public static class Validator<T>
+    public static class Validator
     {
-        delegate bool UserTypeValidator(IUserType<T> cType, T value);
-
-        static Dictionary<Type, UserTypeValidator> slaveMethod;
-
-
-        static Validator()
-        {
-            slaveMethod = new Dictionary<Type, UserTypeValidator>();
-
-            slaveMethod.Add(typeof(long)    , IsValidLong);
-            slaveMethod.Add(typeof(int)     , IsValidInt);
-            slaveMethod.Add(typeof(short)   , IsValidShort);
-            slaveMethod.Add(typeof(byte)    , IsValidByte);
-            slaveMethod.Add(typeof(decimal) , IsValidDecimal);
-            slaveMethod.Add(typeof(float)   , IsValidFloat);
-            slaveMethod.Add(typeof(double)  , IsValidDouble);
-            slaveMethod.Add(typeof(string)  , IsValidString);
-            slaveMethod.Add(typeof(DateTime), IsValidDateTime);
-            slaveMethod.Add(typeof(bool)    , IsValidBool);
-        }
-       
 
         #region - Type Validator 
 
-        static bool IsValidLong(IUserType<T> cType, T value)
+        static bool IsValid(IUserType<long> cType, long value)
         {
-            if (cType.BaseType != typeof(T)) return false;
-
             return false;
         }
 
-        static bool IsValidInt(IUserType<T> cType, T value)
+        static bool IsValid(IUserType<int> cType, int value)
         {
-            if (cType.BaseType != typeof(T)) return false;
-
             return false;
         }
 
-        static bool IsValidShort(IUserType<T> cType, T value)
+        static bool IsValid(IUserType<short> cType, short value)
         {
-            if (cType.BaseType != typeof(T)) return false;
-
             return false;
         }
 
-        static bool IsValidByte(IUserType<T> cType, T value)
+        static bool IsValid(IUserType<byte> cType, byte value)
         {
-            if (cType.BaseType != typeof(T)) return false;
-
             return false;
         }
 
-        static bool IsValidDecimal(IUserType<T> cType, T value)
+        static bool IsValid(IUserType<decimal> cType, decimal value)
         {
-            if (cType.BaseType != typeof(T)) return false;
-
             return false;
         }
 
-        static bool IsValidFloat(IUserType<T> cType, T value)
+        static bool IsValid(IUserType<float> cType, float value)
         {
-            if (cType.BaseType != typeof(T)) return false;
-
             return false;
         }
 
-        static bool IsValidDouble(IUserType<T> cType, T value)
+        static bool IsValid(IUserType<double> cType, double value)
         {
-            if (cType.BaseType != typeof(T)) return false;
-
             return false;
         }
 
-        static bool IsValidString(IUserType<T> cType, T value)
+        static bool IsValid(IUserType<string> cType, string value)
         {
-            if (cType.BaseType != typeof(T)) return false;
-
             return false;
         }
 
-        static bool IsValidDateTime(IUserType<T> cType, T value)
+        static bool IsValid(IUserType<DateTime> cType, DateTime value)
         {
-            if (cType.BaseType != typeof(T)) return false;
-
             return false;
         }
 
-        static bool IsValidBool(IUserType<T> cType, T value)
+        static bool IsValid(IUserType<bool> cType, bool value)
         {
-            if (cType.BaseType != typeof(T)) return false;
-
             return false;
         }
 
         #endregion 
-
-        ////Enum.IsDefined( 
-        public static bool IsValid(IUserType<T> cType, T value)
-        {
-            if (cType == null) throw new ArgumentNullException("cType");            
-
-            try
-            {
-                return slaveMethod[cType.BaseType](cType, value);
-            }
-            catch (KeyNotFoundException e)
-            {
-                throw new InvalidBaseTypeException(cType.BaseType, e);
-            }
-        }
     }
 }
