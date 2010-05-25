b842d735b74e0d6114f3793c990f9820e9f64aff
ppires75 <ppires75@d583e3dc-27d8-11df-bfd4-992fa37f6226>
2010-05-25 16:59:19 +0000

    Alteração no motor do gerador. inclusão da noção de steps

---
+++ Src/EDM/EDM.Generator/Utils/XML/Set.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Xml;
+
+namespace EDM.Generator.Utils.XML
+{
+    internal static class Set
+    {
+        public static void SetAttributeValue(XmlDocument dom, string xPath, string attributeName, string attributeValue)
+        {
+            XmlNode node = Get.GetNode(dom, xPath);
+            if (node != null)
+            {
+                node.Attributes[attributeName].Value = attributeValue;
+            }
+        }
+
+        public static void AddAttribute(XmlDocument dom, string xPath, string attributeName, string attributeValue)
+        {
+            XmlNode node = Get.GetNode(dom, xPath);
+            if (node != null)
+            {
+                XmlAttribute newAttribute = dom.CreateAttribute(attributeName);
+                newAttribute.Value = attributeValue;
+                node.Attributes.Append(newAttribute);
+            }
+        }
+
+        public static void SetNodeText(XmlDocument dom, string xPath, string value)
+        {
+            XmlNode node = Get.GetNode(dom, xPath);
+            if (node != null)
+            {
+                node.InnerText = value;
+            }
+        }
+    }
+}
